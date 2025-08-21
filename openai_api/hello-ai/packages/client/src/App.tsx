e2f2b688d7480cd54d3ff816386b4b70b3a5da45
Nate Stott <nate.stott@pm.me>
2025-08-20 19:44:35 -0600

    set up husky

--- openai_api/hello-ai/packages/client/src/App.tsx
+++ openai_api/hello-ai/packages/client/src/App.tsx
@@ -12,10 +12,13 @@ function App() {
 
     return (
         <div className="p-4">
+            
+             
+            
             <p className="font-bold text-3xl">{message}</p>
-            <Button>Click Me</Button>
+                    <Button>Click Me!</Button>
         </div>
     );
 }
 
-export default App;
+export default App
