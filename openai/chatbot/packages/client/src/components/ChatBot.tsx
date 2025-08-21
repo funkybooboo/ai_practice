eb8fa45481f2fb4641ea7f4fdae4f9ba6f27819a
Nate Stott <nate.stott@pm.me>
2025-08-21 16:34:17 -0600

    make chat bubbles

--- openai/chatbot/packages/client/src/components/ChatBot.tsx
+++ openai/chatbot/packages/client/src/components/ChatBot.tsx
@@ -1,7 +1,8 @@
 import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
+import ReactMarkDown from 'react-markdown';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import { Button } from './ui/button';
 import axios from 'axios';
@@ -10,14 +11,27 @@ type FormData = {
     prompt: string;
 };
 
+type ChatReponse = {
+    message: string;
+};
+
+type Message = {
+    content: string;
+    role: 'user' | 'bot';
+};
+
 const ChatBot = () => {
+    const [messages, setMessages] = useState<Message[]>([]);
     const conversationId = useRef(crypto.randomUUID());
     const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
     const onSubmit = async ({ prompt }: FormData) => {
+        setMessages(prev => [...prev, { content: prompt, role: 'user' }]);
+
         reset();
+
         try {
-            const response = await axios.post(
+            const { data } = await axios.post<ChatReponse>(
                 '/api/chat',
                 {
                     prompt,
@@ -30,9 +44,7 @@ const ChatBot = () => {
                 }
             );
 
-            const data = response.data;
-
-            console.log(data);
+            setMessages(prev => [...prev, { content: data.message, role: 'bot' }]);
         } catch (error) {
             console.log(error);
         }
@@ -46,27 +58,50 @@ const ChatBot = () => {
     };
 
     return (
-        <form
-            onSubmit={handleSubmit(onSubmit)}
-            onKeyDown={onKeyDown}
-            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-        >
-            <textarea
-                {...register('prompt', {
-                    required: true,
-                    validate: (data: string): boolean => data.trim().length > 0,
-                })}
-                className="w-full border-0 focus:outline-0 resize-none"
-                placeholder="Ask anything"
-                maxLength={1000}
-            />
-            <Button
-                disabled={!formState.isValid}
-                className="rounded-full w-9 h-9"
+        <div>
+            <div className='flex flex-col gap-3 mb-10'>
+                {messages.map((message, index) => (
+                    <p 
+                        key={index}
+                        className={`
+                            px-3 py-1 rounded-xl
+                            ${
+                                message.role === 'user' ? 
+                                'bg-blue-600 text-white self-end' : 
+                                'bg-gray-100 text-black self-start'
+                            }
+                        `}
+                    >
+                        <ReactMarkDown>
+                            {message.content}
+                        </ReactMarkDown>
+                    </p>
+                ))}
+            </div>
+
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                onKeyDown={onKeyDown}
+                className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
             >
-                <FaArrowUp />
-            </Button>
-        </form>
+                <textarea
+                    {...register('prompt', {
+                        required: true,
+                        validate: (data: string): boolean =>
+                            data.trim().length > 0,
+                    })}
+                    className="w-full border-0 focus:outline-0 resize-none"
+                    placeholder="Ask anything"
+                    maxLength={1000}
+                />
+                <Button
+                    disabled={!formState.isValid}
+                    className="rounded-full w-9 h-9"
+                >
+                    <FaArrowUp />
+                </Button>
+            </form>
+        </div>
     );
 };
 
