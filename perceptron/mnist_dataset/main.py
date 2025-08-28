227062ed9f6678080bcd0b85b39f0d9a31cbf0fe
Nate Stott <nate.stott@pm.me>
2025-08-28 16:10:26 -0600

    print out training data

--- perceptron/mnist_dataset/main.py
+++ perceptron/mnist_dataset/main.py
@@ -1,7 +1,34 @@
+from os.path import join
+
+from perceptron.mnist_dataset.mnist_dataloader import MnistDataloader
+from perceptron.perceptron import perceptron
 
 
 def main():
-    pass
+    #
+    # Set file paths based on added MNIST Datasets
+    #
+    input_path: str = './archive'
+    training_images_filepath: str = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
+    training_labels_filepath: str = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
+    test_images_filepath: str = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
+    test_labels_filepath: str = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+
+    #
+    # Load MINST dataset
+    #
+    mnist_dataloader: MnistDataloader = MnistDataloader(
+        training_images_filepath,
+        training_labels_filepath,
+        test_images_filepath,
+        test_labels_filepath
+    )
+    (training_data, test_data) = mnist_dataloader.load_data()
+
+    print(training_data)
+
+    
+
 
 if __name__ == "__main__":
     main()
--- perceptron/mnist_dataset/mnist_dataloader.py
+++ perceptron/mnist_dataset/mnist_dataloader.py
@@ -1,96 +1,108 @@
-# got this code from here https://www.kaggle.com/code/hojjatk/read-mnist-dataset
-
-#
-# This is a sample Notebook to demonstrate how to read "MNIST Dataset"
-#
+from __future__ import annotations
 import numpy as np
 import struct
 from array import array
 import random
 import matplotlib.pyplot as plt
 from os.path import join
+from typing import List, Tuple, Union
 
 
 #
 # MNIST Data Loader Class
 #
 class MnistDataloader:
-    def __init__(self,
-        training_images_filepath,
-        training_labels_filepath,
-        test_images_filepath,
-        test_labels_filepath
-     ):
-        self.training_images_filepath = training_images_filepath
-        self.training_labels_filepath = training_labels_filepath
-        self.test_images_filepath = test_images_filepath
-        self.test_labels_filepath = test_labels_filepath
+    def __init__(
+        self,
+        training_images_filepath: str,
+        training_labels_filepath: str,
+        test_images_filepath: str,
+        test_labels_filepath: str
+    ) -> None:
+        self.training_images_filepath: str = training_images_filepath
+        self.training_labels_filepath: str = training_labels_filepath
+        self.test_images_filepath: str = test_images_filepath
+        self.test_labels_filepath: str = test_labels_filepath
 
     @staticmethod
-    def read_images_labels(images_filepath, labels_filepath):
-        labels = []
+    def read_images_labels(
+        images_filepath: str,
+        labels_filepath: str
+    ) -> Tuple[List[np.ndarray], array]:
+        labels: array
         with open(labels_filepath, 'rb') as file:
             magic, size = struct.unpack(">II", file.read(8))
             if magic != 2049:
-                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+                raise ValueError(
+                    f'Magic number mismatch, expected 2049, got {magic}'
+                )
             labels = array("B", file.read())
 
-        image_data = []
+        image_data: array
         with open(images_filepath, 'rb') as file:
             magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
             if magic != 2051:
-                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+                raise ValueError(
+                    f'Magic number mismatch, expected 2051, got {magic}'
+                )
             image_data = array("B", file.read())
 
-        images = []
+        images: List[np.ndarray] = []
         for _ in range(size):
-            images.append([0] * rows * cols)
+            images.append(np.zeros((rows, cols), dtype=np.uint8))
 
         for i in range(size):
-            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
-            img = img.reshape(28, 28)
+            img: np.ndarray = np.array(
+                image_data[i * rows * cols:(i + 1) * rows * cols],
+                dtype=np.uint8
+            ).reshape(28, 28)
             images[i][:] = img
 
         return images, labels
 
-    def load_data(self):
-        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
-        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+    def load_data(
+        self
+    ) -> Tuple[Tuple[List[np.ndarray], array], Tuple[List[np.ndarray], array]]:
+        x_train, y_train = self.read_images_labels(
+            self.training_images_filepath, self.training_labels_filepath
+        )
+        x_test, y_test = self.read_images_labels(
+            self.test_images_filepath, self.test_labels_filepath
+        )
         return (x_train, y_train), (x_test, y_test)
 
 
 #
 # Helper function to show a list of images with their relating titles
 #
-def show_images(images, title_texts):
-    cols = 5
-    rows = int(len(images) / cols) + 1
+def show_images(images: List[np.ndarray], title_texts: List[str]) -> None:
+    cols: int = 5
+    rows: int = int(len(images) / cols) + 1
     plt.figure(figsize=(30, 20))
-    index = 1
-    for x in zip(images, title_texts):
-        image = x[0]
-        title_text = x[1]
+    index: int = 1
+    for image, title_text in zip(images, title_texts):
         plt.subplot(rows, cols, index)
         plt.imshow(image, cmap=plt.cm.gray)
         if title_text != '':
-            plt.title(title_text, fontsize=15);
+            plt.title(title_text, fontsize=15)
         index += 1
+    plt.show()
 
 
-if __name__ == "__main__":
+def main():
     #
     # Set file paths based on added MNIST Datasets
     #
-    input_path = 'archive'
-    training_images_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
-    training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
-    test_images_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
-    test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
+    input_path: str = './archive'
+    training_images_filepath: str = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
+    training_labels_filepath: str = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
+    test_images_filepath: str = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
+    test_labels_filepath: str = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
 
     #
     # Load MINST dataset
     #
-    mnist_dataloader = MnistDataloader(
+    mnist_dataloader: MnistDataloader = MnistDataloader(
         training_images_filepath,
         training_labels_filepath,
         test_images_filepath,
@@ -101,16 +113,21 @@ if __name__ == "__main__":
     #
     # Show some random training and test images
     #
-    images_2_show = []
-    titles_2_show = []
-    for i in range(0, 10):
-        r = random.randint(1, 60000)
-        images_2_show.append(x_train[r])
-        titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))
+    images_2_show: List[np.ndarray] = []
+    titles_2_show: List[str] = []
 
-    for i in range(0, 5):
-        r = random.randint(1, 10000)
+    for _ in range(10):
+        r: int = random.randint(1, 60000)
+        images_2_show.append(x_train[r])
+        titles_2_show.append(f'training image [{r}] = {y_train[r]}')
+
+    for _ in range(5):
+        r: int = random.randint(1, 10000)
         images_2_show.append(x_test[r])
-        titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))
+        titles_2_show.append(f'test image [{r}] = {y_test[r]}')
 
     show_images(images_2_show, titles_2_show)
+
+
+if __name__ == "__main__":
+    main()
